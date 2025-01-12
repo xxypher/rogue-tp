8fbeb4a1653d4894d16be01edd9c0830de8fc37d
xypher <84131859+xxypher@users.noreply.github.com>
2025-01-12 15:53:20 +0200

    Update d.lua

--- d.lua
+++ d.lua
@@ -3,24 +3,20 @@ local localplayer = game.Players.LocalPlayer
 local character = localplayer.Character or localplayer.CharacterAdded:Wait()
 local hrp = character.HumanoidRootPart
 
-print"no trinkets"
-
-if character:FindFirstChild"Right Leg" then
-    character:FindFirstChild"Right Leg":Destroy()
-end
-
 for i,v in pairs(trinkets) do
-
-    if v.Material == Enum.Material.Sandstone then
-        hrp:PivotTo(v.CFrame)
-        if v:FindFirstChild("ClickPart") then
-            print"yh"
-            wait(0.3)
-            if v.ClickPart:FindFirstChild"ClickDetector" then
-                print"suck my nigga"
-                fireclickdetector(v.ClickPart.ClickDetector)
-            end
-        end
-    end
-    print"no trinkets"
+	if v.Name == "Ornament" or v.Name == "Present" then
+		local d = game.TweenService:Create(hrp,TweenInfo.new(3),{
+			Position = v.Position
+		})
+		d:Play()
+		d.Completed:Connect(wait(1))
+		if v:FindFirstChild("ClickPart") then
+			print"yh"
+			wait(0.3)
+			if v.ClickPart:FindFirstChild"ClickDetector" then
+				print"suck my nigga"
+				fireclickdetector(v.ClickPart.ClickDetector)
+			end
+		end
+	end
 end
